369547cdde6effd823e47486cf6f9ff3b84f55da
Tammaro Metalli <jennar.spike@gmail.com>
2026-05-29 17:56:17 +0200

    versione stabile del codice 29  maggio 2026 v2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tammarometalli.movimentazione',
+  appName: 'Movimentazione Metalli',
+  webDir: 'build'
+};
+
+export default config;
